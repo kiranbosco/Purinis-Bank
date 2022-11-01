074233631d48949520338df44d3f37f29894adbe
manoja-git <manu8manup@gmail.com>
2022-11-01 20:27:13 +0530

    added controller

--- src/main/java/com/coding/exercise/bankapp/controller/CustomerController.java
+++ src/main/java/com/coding/exercise/bankapp/controller/CustomerController.java
@@ -1,6 +1,25 @@
 package com.coding.exercise.bankapp.controller;
 
+import com.coding.exercise.bankapp.domain.CustomerDetails;
+import com.coding.exercise.bankapp.service.BankingServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
 
+    private BankingServiceImpl bankingServiceImpl;
+
+    @GetMapping("/all")
+    public List<CustomerDetails> getAllCustomerDetails() {
+        return bankingServiceImpl.findAll();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> addCustomer(@RequestBody CustomerDetails customer) {
+        return this.bankingServiceImpl.addCustomer(customer);
+    }  
 }
