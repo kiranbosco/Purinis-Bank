caac60613ca77e433bc679c841418cef79689ee1
manoja-git <manu8manup@gmail.com>
2022-11-01 20:48:06 +0530

    added controller

--- src/main/java/com/coding/exercise/bankapp/controller/CustomerController.java
+++ src/main/java/com/coding/exercise/bankapp/controller/CustomerController.java
@@ -21,5 +21,15 @@ public class CustomerController {
     @PostMapping("/add")
     public ResponseEntity<Object> addCustomer(@RequestBody CustomerDetails customer) {
         return this.bankingServiceImpl.addCustomer(customer);
-    }  
+    }
+
+    @PutMapping(path = "{/customerNumber}")
+    public ResponseEntity<Object> updateCustomer(@RequestBody CustomerDetails customerDetails, @PathVariable Long customerNumber) {
+        return this.bankingServiceImpl.updateCustomer(customerDetails, customerNumber);
+    }
+
+    @DeleteMapping(path = "{/customerNumber}")
+    public ResponseEntity<Object> deleteCustomerInfo(@PathVariable Long customerNumber) {
+        return this.bankingServiceImpl.deleteCustomer(customerNumber);
+    }
 }
